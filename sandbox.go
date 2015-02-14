2934f9d73bcbe476492f618a6232945b2b1a227b
Nick Landolfi <nclandolfi@gmail.com>
2015-02-13 20:07:06 -0800

    add new models

--- sandbox.go
+++ sandbox.go
@@ -1,10 +1,13 @@
 package stack
 
 import (
+	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/elos/data"
 	"github.com/elos/models/event"
+	"github.com/elos/models/routine"
 	"github.com/elos/models/task"
 	"github.com/elos/models/user"
 )
@@ -61,6 +64,30 @@ func Sandbox(s data.Store) {
 		log.Fatal(err)
 	}
 
+	r, _ := routine.New(s)
+	r.SetID(s.NewID())
+	r.IncludeTask(t)
+	r.IncludeTask(t1)
+	r.IncludeTask(t2)
+
+	log.Print("ActionCount: ", r.ActionCount())
+	access := data.NewAccess(u, s)
+
+	nextAction, _ := r.NextAction(access)
+	u.SetCurrentAction(nextAction)
+	nextAction.SetStartTime(time.Now())
+	u.SetCurrentActionable(r)
+
+	access.Save(r)
+	access.Save(nextAction)
+	access.Save(u)
+
 	log.Printf("User id: %s", u.ID())
 	log.Printf("Event id: %s", e.ID())
+	bytes, err := json.Marshal(u)
+	log.Print(string(bytes))
+	bytes, err = json.Marshal(e)
+	log.Print(string(bytes))
+	bytes, err = json.Marshal(t1)
+	log.Print(string(bytes))
 }
--- store.go
+++ store.go
@@ -4,23 +4,27 @@ import (
 	"log"
 
 	"github.com/elos/data"
-	"github.com/elos/data/mongo"
 	"github.com/elos/models"
+	"github.com/elos/models/action"
 	"github.com/elos/models/event"
+	"github.com/elos/models/routine"
 	"github.com/elos/models/task"
 	"github.com/elos/models/user"
+	"github.com/elos/mongo"
 )
 
 func SetupStore(addr string) data.Store {
-	mongo.RegisterKind(models.UserKind, "users")
-	mongo.RegisterKind(models.EventKind, "events")
-	mongo.RegisterKind(models.TaskKind, "tasks")
 
-	db, err := mongo.NewDB(addr)
-
-	if err != nil {
+	db := mongo.NewDB()
+	if err := db.Connect(addr); err != nil {
 		log.Fatal(err)
 	}
+	db.SetName("test")
+	db.RegisterKind(models.UserKind, "users")
+	db.RegisterKind(models.EventKind, "events")
+	db.RegisterKind(models.TaskKind, "tasks")
+	db.RegisterKind(models.RoutineKind, "routines")
+	db.RegisterKind(models.ActionKind, "actions")
 
 	log.Print("Database connection established")
 
@@ -29,6 +33,8 @@ func SetupStore(addr string) data.Store {
 	s.Register(models.UserKind, user.NewM)
 	s.Register(models.EventKind, event.NewM)
 	s.Register(models.TaskKind, task.NewM)
+	s.Register(models.RoutineKind, routine.NewM)
+	s.Register(models.ActionKind, action.NewM)
 
 	return s
 }
