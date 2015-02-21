b9cb9f52d05b5c42944b745ce1ecc216ec54d649
Nick Landolfi <nclandolfi@gmail.com>
2015-02-21 14:53:38 -0800

    add agents setup to stack

--- agents.go
+++ agents.go
@@ -1,31 +1,30 @@
 package stack
 
 import (
+	"log"
+
 	"github.com/elos/agents"
 	"github.com/elos/data"
 	"github.com/elos/models"
 	"github.com/elos/models/user"
 )
 
-var Outfitter *agents.Outfitter
-
-func SetupServices(s data.Store) {
-	Outfitter = agents.NewOutfitter()
-	go Outfitter.Run()
-
+func SetupAgents(s data.Store) {
 	iter, err := s.NewQuery(models.UserKind).Execute()
+
 	if err != nil {
+		log.Print("shit: err: %s", err.Error())
 	}
 
 	u, _ := user.New(s)
 
 	for iter.Next(u) {
-		agents.OutfitUser(Outfitter, s, u)
 		access := data.NewAccess(u, s)
 		a := agents.NewActionAgent(access, u)
 		go a.Start()
 	}
 
 	if err := iter.Close(); err != nil {
+		log.Print("oh no, err: %s", err.Error())
 	}
 }
--- store.go
+++ store.go
@@ -7,7 +7,10 @@ import (
 	"github.com/elos/models"
 	"github.com/elos/models/action"
 	"github.com/elos/models/event"
+	"github.com/elos/models/fixture"
 	"github.com/elos/models/routine"
+	"github.com/elos/models/schedule"
+	"github.com/elos/models/set"
 	"github.com/elos/models/task"
 	"github.com/elos/models/user"
 	"github.com/elos/mongo"
@@ -25,6 +28,9 @@ func SetupStore(addr string) data.Store {
 	db.RegisterKind(models.TaskKind, "tasks")
 	db.RegisterKind(models.RoutineKind, "routines")
 	db.RegisterKind(models.ActionKind, "actions")
+	db.RegisterKind(models.SetKind, "sets")
+	db.RegisterKind(models.FixtureKind, "fixtures")
+	db.RegisterKind(models.ScheduleKind, "schedules")
 
 	log.Print("Database connection established")
 
@@ -35,6 +41,9 @@ func SetupStore(addr string) data.Store {
 	s.Register(models.TaskKind, task.NewM)
 	s.Register(models.RoutineKind, routine.NewM)
 	s.Register(models.ActionKind, action.NewM)
+	s.Register(models.SetKind, set.NewM)
+	s.Register(models.FixtureKind, fixture.NewM)
+	s.Register(models.ScheduleKind, schedule.NewM)
 
 	return s
 }
